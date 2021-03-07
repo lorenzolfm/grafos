5349eedbf06432dcaa20910e2910f0e43fa8638d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-03-07 13:51:34 -0300

    documentando a classe grafos

--- main.py
+++ main.py
@@ -1,42 +1,97 @@
 import os
 
 """
-Grafo não-dirigido e ponderado
+Grafo não-dirigido e ponderado.
+
+G = (V, E, w)
+
+V -> Conjunto de vértices;
+E -> Conjunto de arestas;
+w -> Função que mapeia o peso de cada aresta {u, v} e E.
 """
 class Grafo:
+    """
+    Construtor
+
+    Parameter: arquivo (.net), como especificado pelo professor.
+    """
     def __init__(self, arquivo):
         self.vertices, self.arestas = self._lerArquivo(arquivo)
 
     """
-    Retorna a quantidade de vértices;
+    Quantidade de Vértices
+
+    Return: Quantidade de vértices do grafo (inteiro).
     """
     def qtdVertices(self):
         return len(self.vertices)
 
     """
-    Retorna a quantidade de arestas;
+    Quantidade de Arestas
+
+
+    Return: Quantidade de arestas (inteiro).
     """
     def qtdArestas(self):
         return len(self.arestas)
 
     """
-    Retorna o grau de um vértice
+    Grau do vértice
+
+    Parameter: vertice (dict).
+    Return: Grau do vértice passado como argumento.
     """
     def grauVertice(self, vertice):
         pass
 
+    """
+    Rótulo
+
+    Parameter: vertice (dict).
+    Return: Rótulo do vértice passado como argumento (TIPO?).
+    """
     def rotulo(self, vertice):
         pass
 
+    """
+    Vizinhos
+
+    Parameter: vertice (dict).
+    Return: Retorna os vizinhos do vértice passado como argumento (List(?))
+    """
     def vizinhos(self, vertice):
         pass
 
+    """
+    Há Aresta?
+
+    Parameter: u, vértice.
+    Parameter: v, vértice.
+    Return: True se há aresta ligando u e v, False caso contrário (bool).
+    """
     def haAresta(self, u, v):
         pass
 
+    """
+    Peso
+
+    Parameter: u, vértice.
+    Parameter: v, vértice.
+    Return: peso da aresta que liga u e v (float).
+    """
     def peso(self, u, v):
         pass
 
+    """
+    Ler Arquivo
+
+    Lê arquivo e retorna duas listas:
+    * vertices
+    * arestas
+
+    Parameter: arquivo (.net)
+    Return: vertices, arestas
+    """
     def _lerArquivo(self, arquivo):
         with open(arquivo) as file:
             vertices = []
@@ -64,10 +119,21 @@ class Grafo:
 
             return vertices, arestas
 
+    """
+    Adiciona Vértice
+
+    Parameter: Vértice no formato "ID ROTULO" (str).
+    Return: Dicionário com as chaves id e rótulo (dict).
+    """
     def _addVertice(self, verticeString):
         vertice = verticeString.split()
         return {"id": int(vertice[0]), "rotulo": vertice[1]}
 
+    """
+    Adiciona Aresta
+
+    Parameter: Aresta no formato "u v w" (vértice, vértice e peso) (str).
+    """
     def _addAresta(self, arestaString):
         aresta = arestaString.split()
         return {"aresta": (int(aresta[0]), int(aresta[1])), "peso": float(aresta[2])}
