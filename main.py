403d3fc15e64179037fb0f335bd6d630e02d16b7
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-03-07 19:28:31 -0300

    pegando número de vértices corretamente

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import os
 from utils.Graph import Graph
 
 grafo = Graph('./assets/fln_pequena.net')
+grafo = Graph('./assets/agm_tiny.net')
--- utils/Graph.py
+++ utils/Graph.py
@@ -1,12 +1,20 @@
 class Graph:
     def __init__(self, file):
+        self._numberOfNodes = 0
+        self._numberOfEdges = 0
+        self._nodes = []
+
         self._readFile(file)
-        self.numberOfNodes = 0
-        self.nodes = []
+
+    def getNumberOfNodes(self):
+        return self._numberOfNodes
 
     def _readFile(self, file):
         with open(file) as f:
             fileData = f.read()
             fileData = fileData.splitlines()
 
-            print(fileData)
+            self._numberOfNodes = self._getNumberOfNodesFrom(fileData)
+
+    def _getNumberOfNodesFrom(self, fileData):
+        return int(fileData[0].split()[1])
