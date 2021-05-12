2c9759c931465f01880ab5095f2e18fa4b30417e
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-05-12 14:39:39 -0300

    try/catch

--- item1.py
+++ item1.py
@@ -1,2 +1,9 @@
+import sys
 from utils.Graph import Graph
 
+try:
+    path = str(sys.argv[1])
+    graph = Graph(path, isDirected = True)
+    # Chamada da função
+except FileNotFoundError:
+    print("Não achei esse arquivo")
