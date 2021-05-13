31f427df7a16d30e6f8ddda8f39e7b84aeb2ea03
Gabriel Carneiro <carneiro.gabr@gmail.com>
2021-05-13 17:27:02 -0300

    questão 1 pronta

--- item1.py
+++ item1.py
@@ -4,9 +4,6 @@ from utils.Graph import Graph
 try:
     path = str(sys.argv[1])
     graph = Graph(path, isDirected = True)
-    s = graph.getNode(1)
-    t = graph.getNode(6)
-    # graph.edmonds_karp(s, t)
-    graph.ford_fulkerson(s, t)
+    graph.ford_fulkerson()
 except FileNotFoundError:
     print("Nao achei esse arquivo")
